74565e4723dd44f5c14ee02d0ffb73f94317889c
ShauryaAkash69 <168525165+ShauryaAkash69@users.noreply.github.com>
2024-06-08 13:33:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,9 +33,9 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "modijiurl.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "aa6d045ba1498980ce3cec37c150b9ab119d0fb4")
-VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 43200)) # Add time in seconds
+VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID","how_to_donload_12/21")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/how_to_donload_12")
 
 
 #force sub channel id, if you want enable force sub
